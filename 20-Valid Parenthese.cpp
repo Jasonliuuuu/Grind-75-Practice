394afbc9cb13133dd253eb679d25a8dbb25becf8
Jasonliuuuu <jason890418123@gmail.com>
2024-12-03 00:01:23 -0600

    Q20 & 21 Finished

---
+++ 20-Valid Parenthese.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        //The function "invalid" check if the input string s contains valid  parenthese. A string is considered valided if 
+        stack <char> st; 
+        for(auto c: s){
+            if (c == '(' || c == '{' || c == '['){
+                st.push(c); 
+            }
+            else if (c == '}' && !st.empty() && st.top() == '{'){
+                st.pop(); 
+            }
+            else if (c == ']' && !st.empty() && st.top() == '['){
+                st.pop(); 
+            }
+            else if (c == ')' && !st.empty() && st.top() == '('){
+                st.pop(); 
+            }
+            else{
+                return false; 
+            }
+            
+        }
+        return st.empty(); 
+    }
+};
